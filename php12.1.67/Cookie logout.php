90a72b057d89d77a561e2106ad865afb60157c9d
WayuOHm99 <121616763+WayuOHm99@users.noreply.github.com>
2024-01-12 16:56:01 +0700

    Create Cookie logout.php

---
+++ php12.1.67/Cookie logout.php
@@ -0,0 +1,25 @@
+<?php
+
+if (isset($_COOKIE['username'])) {
+    $username = $_COOKIE['username'];
+} else {
+    header('Location: cookie.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookie</title>
+</head>
+<body>
+
+    <h1>สวีดัดสวัสดี, <?php echo $username; ?>!</h1>
+    <button type="submit" ><a href="cookie.php">LOGOUT</a>
+
+</body>
+</html>
+
