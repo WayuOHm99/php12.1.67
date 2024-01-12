f606e90c7f5726f74eae8974f9b69040e370335b
WayuOHm99 <121616763+WayuOHm99@users.noreply.github.com>
2024-01-12 16:47:41 +0700

    Update season.php

--- php12.1.67/season.php
+++ php12.1.67/season.php
@@ -2,25 +2,24 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $username = $_POST["username"];
 
     $_SESSION["username"] = $username;
-
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>HARD</title>
 </head>
 <body>
-    <form method="post" action="season.1.php"<?php echo $_SERVER["PHP_SELF"]; ?>>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
-        <button type="submit">Login</button>
-    </form>
+<form action="success.php" method="POST" <?php echo $_SERVER["PHP_SELF"] ?>>
+    <label for="username">USERNAME : </label>
+    <input type="text" name="username" id="username" required>
+    <button type="submit">LOGIN.</button>
+</form>
 </body>
 </html>
