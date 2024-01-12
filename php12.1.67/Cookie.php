1453cd7130d5192bace7a0f21474287ec7e5363b
WayuOHm99 <121616763+WayuOHm99@users.noreply.github.com>
2024-01-12 16:55:30 +0700

    Update Cookie.php

--- php12.1.67/Cookie.php
+++ php12.1.67/Cookie.php
@@ -1,12 +1,12 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
 
-    setcookie("username", $username, time() + 3600, "/");
+    setcookie('username', $username, time() + (2 * 30), "/");
 
-    header("Location: welcome.php");
-    exit();
+    header('Location: cookie-f.php');
+    exit;
 }
 ?>
 
@@ -15,13 +15,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Cookie</title>
 </head>
 <body>
-    <form method="post" action="welcome.php"<?php echo $_SERVER["PHP_SELF"]; ?>">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
-        <button type="submit">Login</button>
+
+    <form method="post" action="">
+        <label for="username">USERNAME :</label>
+        <input type="text" name="username" id="username" required>
+        <button type="submit">LOGIN</button>
     </form>
+
 </body>
 </html>
